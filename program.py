01850882cc4ef79a0c9f372fb25da151d7144d16
domWolfe <dwolfe131@comcast.net>
2025-12-17 12:38:33 -0800

    Updated Algs

--- program.py
+++ program.py
@@ -29,6 +29,8 @@ class ExerciseList:
     
 # 0 = Regular Press, 1 = Upper Chest Press, 2 = Regular Fly, 3 = Upper Chest Fly
 # 0 = Heavy Tricep Lateral, 1 = Heavy Tricep Long, 2 = Tricep Pushdown Variation, 3 = Tricep Overhead Variation
+# 0 = Heavy Rowing Movement, 1 = Heavy Pulling Movement, 2 = Close Grip Rowing Movement, 3 = Traps
+# 0 = Heavy Curls Long, 1 = Heavy Curls Short, 2 = Light Curls Long, 3 = Light Curls Short
 
 chest_exercises = ExerciseList()
 chest_exercises.add_exercise_by_con("Flat Barbell Bench Press", 0)
@@ -55,6 +57,28 @@ triceps_exercises.add_exercise_by_con("Overhead Dumbbell Tricep Extension", 3)
 triceps_exercises.add_exercise_by_con("Overhead Cable Tricep Extension", 3)
 triceps_exercises.add_exercise_by_con("Single Arm Overhead Cable Tricep Extension", 3)
 triceps_exercises.add_exercise_by_con("Single Arm Overhead Dumbbell Tricep Extension", 3)
+
+back_exercises = ExerciseList()
+back_exercises.add_exercise_by_con("Bent Over Barbell Rows", 0)
+back_exercises.add_exercise_by_con("Bent Over Dumbbell Rows", 0)
+back_exercises.add_exercise_by_con("Chest-Supported T-Bar Rows", 0)
+back_exercises.add_exercise_by_con("Weighted Pull-Ups", 1)
+back_exercises.add_exercise_by_con("Wide-Grip Lat-Pulldowns", 1)
+back_exercises.add_exercise_by_con("Seated Cable Row", 2)
+back_exercises.add_exercise_by_con("Seated Cable Row (V-Bar)", 2)
+back_exercises.add_exercise_by_con("Face Pulls", 3)
+back_exercises.add_exercise_by_con("Shrugs", 3)
+
+bicep_exercises = ExerciseList()
+bicep_exercises.add_exercise_by_con("Ez-Bar Curls", 0)
+bicep_exercises.add_exercise_by_con("Dumbbell Curls", 0)
+bicep_exercises.add_exercise_by_con("Preacher Curls", 1)
+bicep_exercises.add_exercise_by_con("Cable Curls", 2)
+bicep_exercises.add_exercise_by_con("Reverse Barbell Curls", 2)
+bicep_exercises.add_exercise_by_con("Incline Dumbbell Curls", 2)
+bicep_exercises.add_exercise_by_con("Hammer Curls", 2)
+bicep_exercises.add_exercise_by_con("Spider Curls", 3)
+bicep_exercises.add_exercise_by_con("Wide-Grip Ez-Bar Curls", 3)
 
 class WorkoutExercises:
     def __init__(self, exercise_list):
@@ -85,12 +109,35 @@ class WorkoutProgram:
         self.program_type = program_type
         self.workouts = {}
     
+    def generate_program(self):
+        if self.program_type == 0:
+            self.generate_arnold_split()
+        elif self.program_type == 1:
+            self.generate_ppl_program()
+
+    def generate_arnold_split(self):
+        chest_workout = WorkoutExercises(chest_exercises)
+        program_chest = chest_workout.generate_exercises()
+        back_workout = WorkoutExercises(back_exercises)
+        program_back = back_workout.generate_exercises()
+        self.workouts["Chest & Back"] = program_chest + program_back
+        bicep_workout = WorkoutExercises(bicep_exercises)
+        program_bicep = bicep_workout.generate_exercises()
+        tricep_workout = WorkoutExercises(triceps_exercises)
+        program_triceps = tricep_workout.generate_exercises()
+        self.workouts["Arms"] = program_triceps + program_bicep
+
     def generate_ppl_program(self):
         chest_workout = WorkoutExercises(chest_exercises)
         program_chest = chest_workout.generate_exercises()
         tricep_workout = WorkoutExercises(triceps_exercises)
         program_triceps = tricep_workout.generate_exercises()
         self.workouts["Push"] = program_chest + program_triceps
+        back_workout = WorkoutExercises(back_exercises)
+        program_back = back_workout.generate_exercises()
+        bicep_workout = WorkoutExercises(bicep_exercises)
+        program_bicep = bicep_workout.generate_exercises()
+        self.workouts["Pull"] = program_back + program_bicep
 
     def print_program(self):
         for workout_name, exercises in self.workouts.items():
@@ -102,3 +149,7 @@ class WorkoutProgram:
 ppl_program = WorkoutProgram(0)
 ppl_program.generate_ppl_program()
 ppl_program.print_program()
+
+arnold_split = WorkoutProgram(1)
+arnold_split.generate_arnold_split()
+arnold_split.print_program()
